fe1c230928f3d075d5411a31a8961eb2a30648b9
DannyN <38981736+ThreshTheRed@users.noreply.github.com>
2024-02-25 16:52:51 +0100

    main.tf updated

--- project_1/main.tf
+++ project_1/main.tf
@@ -1,32 +1,17 @@
-terraform {
- required_providers {
-  aws = {
-   source = "hashicorp/aws"
-   version = "5.37.0"
-   }
-  }
-
- backend "s3" {
-  bucket = "backend_1"
-  key = "terraform.tfstate"
-  region = "eu-central-1"
-  dynamodb_table = "terraform-lock"
-  encrypt = true
- }
- aws_dynamodb_table "terraform-lock" {
-  name = "terraform_state"
-  billing_mode = "PROVISIONED"
-  read_capacity = 5
-  write_capacity = 5
-  hash_key = "LockId"
-  
-  attribute {
-   name = "LockId"
-   type = "S"
-   }
- }  
+resource "aws_s3_bucket""example" {
+	object_lock_enabled = "true"
+	bucket = "danny-test-bucket-es"
 }
 
-provider "aws" {
- region = "eu-central-1"
+resource "aws_dynamodb_table" "terraform-lock" {
+	name = "terraform-state"
+	read_capacity = 5
+	write_capacity = 5
+	hash_key = "LockID"
+	attribute {
+		name = "LockID"
+		type = "S"
+	}
 }
+
+
